f8610270dc51e7f60a73b116e17897b880666a6d
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-10-19 04:35:11 +0300

    feat: call play_even from brain_even script main function

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,10 @@
 from brain_games.cli import welcome_user
+from brain_games.game_even import play_even
 
 
 def main():
     user_name = welcome_user()
-    return user_name
+    play_even(user_name)
 
 
 if __name__ == 'main':
