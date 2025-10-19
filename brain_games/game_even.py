67ce097a851508a3598860d90866bdd511a9ecc0
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-10-19 04:29:20 +0300

    feat: add even number game implementation

---
+++ brain_games/game_even.py
@@ -0,0 +1,35 @@
+from random import randint
+
+import prompt
+
+
+def is_even(num: int) -> bool:
+    return num % 2 == 0
+
+
+# перенесла бы в другой файл для переиспользования
+def get_user_answer():
+    answer = prompt.string('Your answer: ', empty=True)
+    return answer or ''
+
+
+def handle_wrong_answer(user_answer: str, right_answer: str, name: str):
+    print(f'"{user_answer}" is wrong answer ;(.', end=' ')
+    print(f'Correct answer was "{right_answer}".')
+    print(f'Let\'s try again, {name}!')
+
+
+def play_even(user_name: str):
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    for i in range(3):
+        num = randint(1, 100)
+        print(f'Question: {num}')
+        user_answer = get_user_answer()
+        right_answer = 'yes' if is_even(num) else 'no'
+        if user_answer != right_answer:
+            handle_wrong_answer(user_answer, right_answer, user_name)
+            return
+        print('Correct!')
+
+    print(f'Congratulations, {user_name}!')
