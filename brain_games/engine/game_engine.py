ecbb3b1d93817551982b1db2c2ba0a6bb22b023b
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-10-20 11:42:27 +0300

    feat: use get_two_random_nums and calculate_gcd in game_engine

--- brain_games/engine/game_engine.py
+++ brain_games/engine/game_engine.py
@@ -2,15 +2,17 @@ import prompt
 
 from brain_games.games.calc import get_expression, solve_expression
 from brain_games.games.even import get_random_int, is_even
+from brain_games.games.gcd import calculate_gcd, get_two_random_nums
 
 
 def print_intro(game):
     match game:
         case 'even':
             print('Answer "yes" if the number is even, otherwise answer "no".')
-            return
         case 'calc':
             print('What is the result of the expression?')
+        case 'gcd':
+            print('Find the greatest common divisor of given numbers.')
 
 
 def get_question(game: str) -> int | str | None:
@@ -19,6 +21,9 @@ def get_question(game: str) -> int | str | None:
             return get_random_int()
         case 'calc':
             return get_expression()
+        case 'gcd':
+            (first_num, second_num) = get_two_random_nums()
+            return f'{first_num} {second_num}'
 
 
 def ask_question(question: str) -> None:
@@ -33,17 +38,18 @@ def get_right_answer(game: str, question: str) -> str | int | None:
             [operand_1, operator_sign, operand_2] = question.split(' ')
             return solve_expression(
                 int(operand_1), operator_sign, int(operand_2))
+        case 'gcd':
+            [first_num, second_num] = question.split(' ')
+            return calculate_gcd(int(first_num), int(second_num))
 
 
 def get_user_answer(game: str) -> str | int | None:
-    answer = ''
     message = 'Your answer: '
     match game:
         case 'even':
-            answer = prompt.string(message)
-        case 'calc':
-            answer = prompt.integer(message)
-    return answer
+            return prompt.string(message)
+        case _:
+            return prompt.integer(message)
 
 
 def handle_wrong_answer(
