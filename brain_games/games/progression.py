2730d7d5f4526f227c07b00e52905c4d7e078366
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-10-20 13:50:31 +0300

    feat: write func to create progression and func to hide random num in progression

---
+++ brain_games/games/progression.py
@@ -0,0 +1,16 @@
+from random import randint
+
+
+def make_progression() -> list[str]:
+    start = randint(-10, 50)
+    step = randint(-10, 10)
+    length = randint(5, 10)
+    return [str(start + i * step) for i in range(length - 1)]
+
+
+def hide_num_in_progresion(progression: list) -> tuple:
+    length = len(progression)
+    rand_i = randint(0, length - 1)
+    hidden_num = int(progression[rand_i])
+    result = progression[0:rand_i] + ['..'] + progression[rand_i:length]
+    return (" ".join(result), hidden_num)
