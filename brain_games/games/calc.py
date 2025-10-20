50125f690563dd83d82cf408672c91bb19ba221d
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-10-20 13:45:52 +0300

    feat: get_expression returns list

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,7 +2,7 @@ from operator import add, mul, sub
 from random import randint
 
 
-def get_expression() -> str:
+def get_expression() -> list:
     operators = ['+', '-', '*']
     random_i = randint(0, len(operators) - 1)
     operator_sign = operators[random_i]
@@ -13,7 +13,7 @@ def get_expression() -> str:
         case _:
             operand_1 = randint(0, 50)
             operand_2 = randint(0, 50)
-    return f'{operand_1} {operator_sign} {operand_2}'
+    return [operand_1, operator_sign, operand_2]
 
 
 def solve_expression(operand_1: int, operator_sign: str, operand_2: int) -> int:
