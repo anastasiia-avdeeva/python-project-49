b10ea0c51eb4c6fa9ef52f5df967dd16ea16dad4
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-10-20 11:45:08 +0300

    feat: create script for brain-gcd

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,11 @@
+from brain_games.cli import welcome_user
+from brain_games.engine.game_engine import play
+
+
+def main():
+    user_name = welcome_user()
+    play(user_name, 'gcd')
+
+
+if __name__ == '__main__':
+    main()
