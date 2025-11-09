51b7602cc1da772570452edfad296450c26d9b71
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-11-09 15:13:22 +0200

    refactor(calc): separate game logic by writing functions for getting rules and forming question/answer

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -20,3 +20,14 @@ def solve_expression(operand_1: int, operator_sign: str, operand_2: int) -> int:
     operators_dict = {'+': add, '-': sub, '*': mul}
     answer = operators_dict[operator_sign](operand_1, operand_2)
     return answer
+
+
+def get_rules() -> str:
+    return 'What is the result of the expression?'
+
+
+def get_question_and_answer() -> tuple[str, str]:
+    [operand_1, operator_sign, operand_2] = get_expression()
+    question = f'{operand_1} {operator_sign} {operand_2}'
+    answer = solve_expression(operand_1, operator_sign, operand_2)
+    return question, str(answer)
