cf6256edd130c9f51b8fa5ac05b51c289c02b37a
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-11-09 15:14:40 +0200

    refactor(calc): small fixes

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,7 +2,7 @@ from operator import add, mul, sub
 from random import randint
 
 
-def get_expression() -> list:
+def get_expression() -> tuple:
     operators = ['+', '-', '*']
     random_i = randint(0, len(operators) - 1)
     operator_sign = operators[random_i]
@@ -13,7 +13,7 @@ def get_expression() -> list:
         case _:
             operand_1 = randint(0, 50)
             operand_2 = randint(0, 50)
-    return [operand_1, operator_sign, operand_2]
+    return (operand_1, operator_sign, operand_2)
 
 
 def solve_expression(operand_1: int, operator_sign: str, operand_2: int) -> int:
@@ -27,7 +27,7 @@ def get_rules() -> str:
 
 
 def get_question_and_answer() -> tuple[str, str]:
-    [operand_1, operator_sign, operand_2] = get_expression()
+    (operand_1, operator_sign, operand_2) = get_expression()
     question = f'{operand_1} {operator_sign} {operand_2}'
     answer = solve_expression(operand_1, operator_sign, operand_2)
     return question, str(answer)
