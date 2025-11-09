d0ca1440674890e725ecbcdd305135290a12285b
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-11-09 15:10:33 +0200

    refactor(even): separate game logic by writing module's own functions for getting rules and forming question/answer

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,2 +1,15 @@
+from random import randint
+
+
 def is_even(num: int) -> bool:
     return num % 2 == 0
+
+
+def get_rules() -> str:
+    return 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def get_question_and_answer() -> tuple[str, str]:
+    question = randint(1, 100)
+    answer = 'yes' if is_even(question) else 'no'
+    return str(question), answer
